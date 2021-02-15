d60cc3ba08caf968abc493ba704a99c24d0de18d
anti-malware <restuwahyu13@gmail.com>
2021-02-16 01:35:37 +0700

    update all config

---
+++ companies-service/jest.config.ts
@@ -0,0 +1,32 @@
+import { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+	preset: "ts-jest",
+	testEnvironment: "node",
+	moduleFileExtensions: ["ts"],
+	cache: true,
+	testMatch: [
+		"<rootDir>/__tests__/**/*.{test.ts,spec.ts}",
+		"<rootDir>/tests/**/*.{test.ts,spec.ts}",
+	],
+	testPathIgnorePatterns: [
+		"node_modules",
+		"dists",
+		".dockerignore",
+		".editorconfig",
+		".env",
+		".eslintignore",
+		".eslintrc",
+		".gitignore",
+		".prettierignore",
+		".prettierrc",
+		"companies.mk",
+		"Dockerfile.dev",
+		"Dockerfile.prod",
+		"package.lock.json",
+		"package.json",
+		"tsconfig.json",
+	],
+};
+
+export default config;
