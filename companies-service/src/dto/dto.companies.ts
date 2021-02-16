03bde93bdb476a7792fe0b174193f093474cc3e8
anti-malware <restuwahyu13@gmail.com>
2021-02-17 00:18:43 +0700

    update dto and model

---
+++ companies-service/src/dto/dto.companies.ts
@@ -0,0 +1,21 @@
+import { Document } from 'mongoose'
+
+export class CompaniesDTO extends Document {
+	readonly companiesId?: String
+	readonly name: String
+	readonly email: String
+	readonly password: String
+	readonly location: String
+	readonly phone: Number
+	readonly photo?: String
+	readonly bannerPhoto?: String
+	readonly industry?: String
+	readonly overview?: String
+	readonly gallery?: string[]
+	readonly role?: String
+	readonly active?: Boolean
+	readonly firstLogin?: any
+	readonly lastLogin?: any
+	readonly createdAt?: any
+	readonly updatedAt?: any
+}
--- profiles-service/src/dto/dto.profile.ts
+++ profiles-service/src/dto/dto.profile.ts
@@ -3,17 +3,17 @@ import { Document } from 'mongoose'
 export class ProfilesDTO extends Document {
 	readonly profileId?: string
 	readonly userId: string
-	readonly photo: string
-	readonly birthDate: any
-	readonly status: string
-	readonly nationality: string
-	readonly aboutMe: string
-	readonly workExperience: number
-	readonly education: Record<string, any>
-	readonly skill: string[]
-	readonly interest: Record<string, any>
-	readonly resume: string
-	readonly portofolio: Record<string, any>
-	readonly appreciation: Record<string, any>
-	readonly volunteerExperience: Record<string, any>
+	readonly photo?: string
+	readonly birthDate?: any
+	readonly status?: string
+	readonly nationality?: string
+	readonly aboutMe?: string
+	readonly workExperience?: number
+	readonly education?: Record<string, any>
+	readonly skills?: string[]
+	readonly interest?: Record<string, any>
+	readonly resume?: string
+	readonly portofolio?: Record<string, any>
+	readonly appreciation?: Record<string, any>
+	readonly volunteerExperience?: Record<string, any>
 }
