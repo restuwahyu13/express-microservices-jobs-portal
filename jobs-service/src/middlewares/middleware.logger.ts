79d6259fddd11b32642eeb437da3a1efda3940bd
restuwahyu13 <restuwahyu13@gmail.com>
2021-02-21 18:52:25 +0700

    update logger middleware to all service

--- jobs-service/src/middlewares/middleware.logger.ts
+++ jobs-service/src/middlewares/middleware.logger.ts
@@ -1,31 +1,45 @@
-res.on('finish', () => {
-	if (res.statusCode >= 400) {
-		consola.info(
-			chalk.red(
-				'[Logger]:',
-				JSON.stringify({
-					serviceName: serviceName,
-					ipAddress: req.ip || req.headers['x-forwarded-host'] || req.connection.remoteAddress,
-					path: req.url,
-					method: req.method,
-					status: res.statusCode,
-					timestamp: dateFormat(new Date()).format('LLLL')
-				})
-			)
-		)
-	} else {
-		consola.info(
-			chalk.green(
-				'[Logger]:',
-				JSON.stringify({
-					serviceName: serviceName,
-					ipAddress: req.ip || req.headers['x-forwarded-host'] || req.connection.remoteAddress,
-					path: req.url,
-					method: req.method,
-					status: res.statusCode,
-					timestamp: dateFormat(new Date()).format('LLLL')
-				})
-			)
-		)
+import { Request, Response, NextFunction } from 'express'
+import consola from 'consola'
+import chalk from 'chalk'
+import { dateFormat } from '../utils/util.date'
+
+export const serviceLogger = (serviceName: string) => (req: Request, res: Response, next: NextFunction) => {
+	if (process.env.NODE_ENV !== 'production') {
+		customLogger(req, res, serviceName)
+		next()
 	}
-})
+}
+
+const customLogger = (req: Request, res: Response, serviceName: string) => {
+	res.on('finish', () => {
+		if (res.statusCode >= 400) {
+			consola.info(
+				chalk.red(
+					'[Logger]:',
+					JSON.stringify({
+						serviceName: serviceName,
+						ipAddress: req.ip || req.headers['x-forwarded-host'] || res.connection.remoteAddress,
+						path: req.url,
+						method: req.method,
+						status: res.statusCode,
+						timestamp: dateFormat(new Date()).format('LLLL')
+					})
+				)
+			)
+		} else {
+			consola.info(
+				chalk.green(
+					'[Logger]:',
+					JSON.stringify({
+						serviceName: serviceName,
+						ipAddress: req.ip || req.headers['x-forwarded-host'] || res.connection.remoteAddress,
+						path: req.url,
+						method: req.method,
+						status: res.statusCode,
+						timestamp: dateFormat(new Date()).format('LLLL')
+					})
+				)
+			)
+		}
+	})
+}
--- users-service/src/middlewares/middleware.logger.ts
+++ users-service/src/middlewares/middleware.logger.ts
@@ -18,7 +18,7 @@ const customLogger = (req: Request, res: Response, serviceName: string) => {
 					'[Logger]:',
 					JSON.stringify({
 						serviceName: serviceName,
-						ipAddress: req.ip || req.headers['x-forwarded-host'] || req.connection.remoteAddress,
+						ipAddress: req.ip || req.headers['x-forwarded-host'] || res.connection.remoteAddress,
 						path: req.url,
 						method: req.method,
 						status: res.statusCode,
@@ -32,7 +32,7 @@ const customLogger = (req: Request, res: Response, serviceName: string) => {
 					'[Logger]:',
 					JSON.stringify({
 						serviceName: serviceName,
-						ipAddress: req.ip || req.headers['x-forwarded-host'] || req.connection.remoteAddress,
+						ipAddress: req.ip || req.headers['x-forwarded-host'] || res.connection.remoteAddress,
 						path: req.url,
 						method: req.method,
 						status: res.statusCode,
