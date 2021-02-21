d6456f0ba1f2218cbeb1b6b626f02b705dcc549f
restuwahyu13 <restuwahyu13@gmail.com>
2021-02-21 14:31:58 +0700

    update all files

--- users-service/src/utils/util.subscriber.ts
+++ users-service/src/utils/util.subscriber.ts
@@ -18,23 +18,23 @@ export class Subscriber {
 			this.serviceName,
 			async (job) => {
 				if (job.name == this.listenerName) {
-					this.queueEvent.emit(this.listenerName, JSON.stringify({ data: job.data }))
+					await this.queueEvent.emit(this.listenerName, JSON.stringify({ data: job.data }))
 				}
 			},
-			{ limiter: { duration: 3000, max: 25 } }
+			{ limiter: { duration: 1000, max: 25 } }
 		)
 	}
 
-	private _notifications(): void {
-		this._worker()
+	private async _notifications(): Promise<void> {
+		await this._worker()
 		this.queueEvent.on('completed', (job) => consola.success(`${this.listenerName} completed ${job.jobId}`))
 		this.queueEvent.on('waiting', (job) => consola.info(`${this.listenerName} waiting ${job.jobId}`))
 		this.queueEvent.on('active', (job) => consola.info(`${this.listenerName} active ${job.jobId}`))
 		this.queueEvent.on('failed', (job) => consola.error(`${this.listenerName} failed ${job.jobId}`))
 	}
 
-	listener(): Promise<Record<string, any>> {
-		this._notifications()
+	async listener(): Promise<Record<string, any>> {
+		await this._notifications()
 		return new Promise((resolve, _) => {
 			this.queueEvent.once(this.listenerName, async (data) => {
 				resolve(JSON.parse(data).data)
