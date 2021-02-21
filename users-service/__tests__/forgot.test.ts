c4623f31150177c6f7902f943a41fdb4b15fec44
restuwahyu13 <restuwahyu13@gmail.com>
2021-02-21 23:57:31 +0700

    update forgot.test.ts

--- users-service/__tests__/forgot.test.ts
+++ users-service/__tests__/forgot.test.ts
@@ -15,4 +15,30 @@ describe('FORGOT.ts', () => {
 		await new QueueEvents('forgot').close()
 		done()
 	})
+
+	it('get statusCode email is valid and forgot password successfully', async (done) => {
+		const res: Response = await request(app)
+			.post(`/api/v1/user/forgot-password`)
+			.send({ email: 'restuwahyu13#zetmail.com' })
+			.set('Content-Type', 'application/json')
+			.expect(200)
+
+		expect(res.body.method).toEqual('POST')
+		expect(res.body.status).toEqual(200)
+		expect(res.body.message).toEqual('change new password successfully, please login')
+		done()
+	})
+
+	it('get statusCode email is not valid', async (done) => {
+		const res: Response = await request(app)
+			.post(`/api/v1/user/forgot-password`)
+			.send({ email: 'restuwahyu13#zetmail.com' })
+			.set('Content-Type', 'application/json')
+			.expect(400)
+
+		expect(res.body.method).toEqual('POST')
+		expect(res.body.status).toEqual(400)
+		expect(res.body.errors[0].msg).toEqual('email is not valid')
+		done()
+	})
 })
