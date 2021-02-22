05130efe906fcf4bee842875e3ac56ea42a3efa1
restuwahyu13 <restuwahyu13@gmail.com>
2021-02-22 14:47:04 +0700

    update profiles service

---
+++ profiles-service/src/app.ts
@@ -0,0 +1,14 @@
+import express, { Express } from 'express'
+import { mongooseConnection } from './utils/util.connection'
+import { bullDashboardMonitor } from './utils/util.dashboard'
+import { routeMiddleware } from './middlewares/middleware.route'
+import { pluginMiddleware } from './middlewares/middleware.plugin'
+
+const app = express() as Express
+
+mongooseConnection()
+bullDashboardMonitor()
+pluginMiddleware(app)
+routeMiddleware(app)
+
+export default app
