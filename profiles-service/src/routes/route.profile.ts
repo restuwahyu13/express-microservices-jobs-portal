6b0b4dddac84302c0449831c7142cf9d0c810adf
Anti-Malware <restuwahyu13@gmail.com>
2021-03-05 15:58:38 +0700

    add logger middleware to all route

--- profiles-service/src/routes/route.profile.ts
+++ profiles-service/src/routes/route.profile.ts
@@ -1,29 +1,58 @@
 import express, { Router } from 'express'
 import { controller } from '../controllers'
 import { fileUpload } from '../utils/util.upload'
+import { serviceLogger } from '../middlewares/middleware.logger'
 
 const router = express.Router() as Router
 
-router.post('/users/:userId/profile', fileUpload.fields([{ name: 'image' }, { name: 'document' }]), controller.meCreateController)
-router.get('/users/:userId/profile/me', controller.meResultController)
-router.delete('/users/:userId/profile/me', controller.meDeleteController)
+router.post(
+	'/users/:userId/profile',
+	[serviceLogger('Skills Service'), fileUpload.fields([{ name: 'image' }, { name: 'document' }])],
+	controller.meCreateController
+)
+router.get('/users/:userId/profile/me', [serviceLogger('Me Service')], controller.meResultController)
+router.delete('/users/:userId/profile/me', [serviceLogger('Me Service')], controller.meDeleteController)
 router.put(
 	'/users/:userId/profile/me',
 	fileUpload.fields([{ name: 'image' }, { name: 'document' }]),
 	controller.meUpdateController
 )
-router.post('/users/:userId/profile/skills', controller.skillsDeleteController)
-router.post('/users/:userId/profile/educations/:educationId', controller.educationsDeleteController)
-router.post('/users/:userId/profile/works/:workId', controller.worksDeleteController)
-router.post('/users/:userId/profile/volunteers/:volunterId', controller.volunteersDeleteController)
-router.post('/users/:userId/profile/appreciations/:appreciationId', controller.appreciationsDeleteController)
-router.post('/users/:userId/profile/jobs/:jobId', controller.jobsDeleteController)
+router.post('/users/:userId/profile/skills', [serviceLogger('Skills Service')], controller.skillsDeleteController)
+router.post(
+	'/users/:userId/profile/educations/:educationId',
+	[serviceLogger('Educations Service')],
+	controller.educationsDeleteController
+)
+router.post('/users/:userId/profile/works/:workId', [serviceLogger('Works Service')], controller.worksDeleteController)
+router.post(
+	'/users/:userId/profile/volunteers/:volunterId',
+	[serviceLogger('Volunteers Service')],
+	controller.volunteersDeleteController
+)
+router.post(
+	'/users/:userId/profile/appreciations/:appreciationId',
+	[serviceLogger('Appreciations Service')],
+	controller.appreciationsDeleteController
+)
+router.post('/users/:userId/profile/jobs/:jobId', [serviceLogger('Jobs Service')], controller.jobsDeleteController)
 // router.put('/users/:userId/profile/me', controller.updateController)
-router.put('/users/:userId/profile/skills', controller.skillsUpdateController)
-router.put('/users/:userId/profile/educations/:educationId', controller.educationsUpdateController)
-router.put('/users/:userId/profile/works/:workId', controller.worksUpdateController)
-router.put('/users/:userId/profile/volunteers/:volunterId', controller.volunteersUpdateController)
-router.put('/users/:userId/profile/appreciations/:appreciationId', controller.appreciationsUpdateController)
-router.put('/users/:userId/profile/jobs/:jobId', controller.jobsUpdateController)
+router.put('/users/:userId/profile/skills', [serviceLogger('Skills Service')], controller.skillsUpdateController)
+router.put(
+	'/users/:userId/profile/educations/:educationId',
+	[serviceLogger('Educations Service')],
+	controller.educationsUpdateController
+)
+router.put('/users/:userId/profile/works/:workId', [serviceLogger('Works Service')], controller.worksUpdateController)
+router.put(
+	'/users/:userId/profile/volunteers/:volunterId',
+	[serviceLogger('Volunteers Service')],
+	controller.volunteersUpdateController
+)
+router.put(
+	'/users/:userId/profile/appreciations/:appreciationId',
+	[serviceLogger('Appreciations Service')],
+	controller.appreciationsUpdateController
+)
+router.put('/users/:userId/profile/jobs/:jobId', [serviceLogger('Jobs Service')], controller.jobsUpdateController)
 
 export default router
