c8db6767dd9af5840cd8a5a685487e716c79e36a
Anti-Malware <restuwahyu13@gmail.com>
2021-03-05 16:15:58 +0700

    add auth middleware and add jwt custom method

--- profiles-service/src/routes/route.profile.ts
+++ profiles-service/src/routes/route.profile.ts
@@ -2,57 +2,58 @@ import express, { Router } from 'express'
 import { controller } from '../controllers'
 import { fileUpload } from '../utils/util.upload'
 import { serviceLogger } from '../middlewares/middleware.logger'
+import { authJwt } from '../middlewares/middleware.auth'
 
 const router = express.Router() as Router
 
 router.post(
 	'/users/:userId/profile',
-	[serviceLogger('Skills Service'), fileUpload.fields([{ name: 'image' }, { name: 'document' }])],
+	[serviceLogger('Skills Service'), authJwt(), fileUpload.fields([{ name: 'image' }, { name: 'document' }])],
 	controller.meCreateController
 )
-router.get('/users/:userId/profile/me', [serviceLogger('Me Service')], controller.meResultController)
-router.delete('/users/:userId/profile/me', [serviceLogger('Me Service')], controller.meDeleteController)
+router.get('/users/:userId/profile/me', [serviceLogger('Me Service'), authJwt()], controller.meResultController)
+router.delete('/users/:userId/profile/me', [serviceLogger('Me Service'), authJwt()], controller.meDeleteController)
 router.put(
 	'/users/:userId/profile/me',
-	fileUpload.fields([{ name: 'image' }, { name: 'document' }]),
+	[serviceLogger('Me Service'), authJwt(), fileUpload.fields([{ name: 'image' }, { name: 'document' }])],
 	controller.meUpdateController
 )
-router.post('/users/:userId/profile/skills', [serviceLogger('Skills Service')], controller.skillsDeleteController)
+router.post('/users/:userId/profile/skills', [serviceLogger('Skills Service'), authJwt()], controller.skillsDeleteController)
 router.post(
 	'/users/:userId/profile/educations/:educationId',
-	[serviceLogger('Educations Service')],
+	[serviceLogger('Educations Service'), authJwt()],
 	controller.educationsDeleteController
 )
-router.post('/users/:userId/profile/works/:workId', [serviceLogger('Works Service')], controller.worksDeleteController)
+router.post('/users/:userId/profile/works/:workId', [serviceLogger('Works Service'), authJwt()], controller.worksDeleteController)
 router.post(
 	'/users/:userId/profile/volunteers/:volunterId',
-	[serviceLogger('Volunteers Service')],
+	[serviceLogger('Volunteers Service'), authJwt()],
 	controller.volunteersDeleteController
 )
 router.post(
 	'/users/:userId/profile/appreciations/:appreciationId',
-	[serviceLogger('Appreciations Service')],
+	[serviceLogger('Appreciations Service'), authJwt()],
 	controller.appreciationsDeleteController
 )
-router.post('/users/:userId/profile/jobs/:jobId', [serviceLogger('Jobs Service')], controller.jobsDeleteController)
+router.post('/users/:userId/profile/jobs/:jobId', [serviceLogger('Jobs Service'), authJwt()], controller.jobsDeleteController)
 // router.put('/users/:userId/profile/me', controller.updateController)
-router.put('/users/:userId/profile/skills', [serviceLogger('Skills Service')], controller.skillsUpdateController)
+router.put('/users/:userId/profile/skills', [serviceLogger('Skills Service'), authJwt()], controller.skillsUpdateController)
 router.put(
 	'/users/:userId/profile/educations/:educationId',
-	[serviceLogger('Educations Service')],
+	[serviceLogger('Educations Service'), authJwt()],
 	controller.educationsUpdateController
 )
-router.put('/users/:userId/profile/works/:workId', [serviceLogger('Works Service')], controller.worksUpdateController)
+router.put('/users/:userId/profile/works/:workId', [serviceLogger('Works Service'), authJwt()], controller.worksUpdateController)
 router.put(
 	'/users/:userId/profile/volunteers/:volunterId',
-	[serviceLogger('Volunteers Service')],
+	[serviceLogger('Volunteers Service'), authJwt()],
 	controller.volunteersUpdateController
 )
 router.put(
 	'/users/:userId/profile/appreciations/:appreciationId',
-	[serviceLogger('Appreciations Service')],
+	[serviceLogger('Appreciations Service'), authJwt()],
 	controller.appreciationsUpdateController
 )
-router.put('/users/:userId/profile/jobs/:jobId', [serviceLogger('Jobs Service')], controller.jobsUpdateController)
+router.put('/users/:userId/profile/jobs/:jobId', [serviceLogger('Jobs Service'), authJwt()], controller.jobsUpdateController)
 
 export default router
