ce4ad429544c5e68e1f1f3246982ea2de2a92689
Anti-Malware <restuwahyu13@gmail.com>
2021-03-09 00:33:08 +0700

    update model from jobs service

--- jobs-service/src/dto/dto.job.ts
+++ jobs-service/src/dto/dto.job.ts
@@ -1,6 +1,6 @@
 import { Document } from 'mongoose'
 
-class JobsDTO extends Document {
+export class JobsDTO extends Document {
 readonly jobsId?: string
 readonly companiesId?: string
 readonly position?: string
@@ -8,7 +8,7 @@ readonly category?: string
 readonly workingTime?: string
 readonly experince?: string
 readonly description?: string
-readonly skills?: []string
+readonly skills?: string[]
 readonly createdAt?: any
 readonly updatedAt?: any
 }
--- jobs-service/src/models/model.job.ts
+++ jobs-service/src/models/model.job.ts
@@ -1,51 +1,51 @@
-import mongoose, { Model, Schema } from 'mongoose'
+import mongoose, { Model } from 'mongoose'
 import { v4 as uuid } from 'uuid'
-import { JobsDTO } from './dto.jobs'
+import { JobsDTO } from '../dto/dto.job'
 
-const Schema = new Schema({
+const Schema = new mongoose.Schema({
  jobsId: {
  	type: String,
- 	unique: [true, 'jobsId must be a unique']
- 	trim: true
- 	required: [true, 'jobsId is required']
+ 	unique: [true, 'jobsId must be a unique'],
+ 	trim: true,
+ 	required: [true, 'jobsId is required'],
  	default: uuid()
  },
  companiesId: {
  	type: String,
- 	unique: [true, 'jobsId must be a unique']
- 	trim: true
+ 	unique: [true, 'jobsId must be a unique'],
+ 	trim: true,
  	ref: "companiesService",
  	default: null
  },
  position: {
  	type: String,
- 	trim: true
+ 	trim: true,
  	required: [true, 'position is required'],
- 		default: null
+ 	default: null
  },
  category: {
  	type: String,
- 	trim: true
+ 	trim: true,
  	required: [true, 'category is required'],
- 		default: null
+ 	default: null
  },
  workingTime: {
  	type: String,
- 	trim: true
+ 	trim: true,
  	required: [true, 'workingTime is required'],
- 		default: null
+ 	default: null
  },
  experince: {
  	type: String,
- 	trim: true
+ 	trim: true,
  	required: [true, 'experince is required'],
- 		default: null
+ 	default: null
  },
  description: {
  	type: String,
- 	trim: true
+ 	trim: true,
  	required: [true, 'description is required'],
- 		default: null
+ 	default: null
  },
   createdAt: {
  	type: Date,
