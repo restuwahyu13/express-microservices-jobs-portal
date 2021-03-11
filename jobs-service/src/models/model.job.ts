7f737ba122fbce61fbda88c0d7ff31cd0cc20eae
restuwahyu13 <restuwahyu13@gmail.com>
2021-03-11 14:04:44 +0700

    update jobs model from jobs service

--- jobs-service/src/models/model.job.ts
+++ jobs-service/src/models/model.job.ts
@@ -3,58 +3,92 @@ import { v4 as uuid } from 'uuid'
 import { JobsDTO } from '../dto/dto.job'
 
 const Schema = new mongoose.Schema({
- jobsId: {
- 	type: String,
- 	unique: [true, 'jobsId must be a unique'],
- 	trim: true,
- 	required: [true, 'jobsId is required'],
- 	default: uuid()
- },
- companiesId: {
- 	type: String,
- 	unique: [true, 'jobsId must be a unique'],
- 	trim: true,
- 	ref: "companiesService",
- 	default: null
- },
- position: {
- 	type: String,
- 	trim: true,
- 	required: [true, 'position is required'],
- 	default: null
- },
- category: {
- 	type: String,
- 	trim: true,
- 	required: [true, 'category is required'],
- 	default: null
- },
- workingTime: {
- 	type: String,
- 	trim: true,
- 	required: [true, 'workingTime is required'],
- 	default: null
- },
- experince: {
- 	type: String,
- 	trim: true,
- 	required: [true, 'experince is required'],
- 	default: null
- },
- description: {
- 	type: String,
- 	trim: true,
- 	required: [true, 'description is required'],
- 	default: null
- },
-  createdAt: {
- 	type: Date,
- 	default: null
- },
-  updatedAt: {
- 	type: Date,
- 	default: null
- }
+	jobsId: {
+		type: String,
+		unique: [true, 'jobsId must be a unique'],
+		trim: true,
+		required: [true, 'jobsId is required'],
+		default: uuid()
+	},
+	companiesId: {
+		type: String,
+		unique: [true, 'companiesId must be a unique'],
+		trim: true,
+		required: [true, 'companiesId is required'],
+		ref: 'companiesService',
+		default: null
+	},
+	jobsVancyUsers: [
+		{
+			userId: {
+				type: mongoose.Types.Array,
+				unique: [true, 'userId must be a unique'],
+				trim: true,
+				ref: 'usersService',
+				default: []
+			},
+			jobsApplicationStatus: {
+				type: String,
+				trim: true,
+				default: null
+			},
+			jobsApplicationDescription: {
+				type: String,
+				trim: true,
+				default: null
+			}
+		}
+	],
+	jobsVancyPosition: {
+		type: String,
+		trim: true,
+		required: [true, 'jobsVancyPosition is required'],
+		default: null
+	},
+	jobsVancyCategory: {
+		type: String,
+		trim: true,
+		required: [true, 'jobsVancyCategory is required'],
+		default: null
+	},
+	jobsVancyWorkingTime: {
+		type: String,
+		trim: true,
+		required: [true, 'jobsVancyWorkingTime is required'],
+		default: null
+	},
+	jobsVancyExperince: {
+		type: String,
+		trim: true,
+		required: [true, 'jobsVancyExperince is required'],
+		default: null
+	},
+	jobsVancyStatus: {
+		type: String,
+		trim: true,
+		required: [true, 'jobsVancyStatus is required'],
+		default: null
+	},
+	jobsVancyDescription: {
+		type: String,
+		trim: true,
+		required: [true, 'jobsVancyDescription is required'],
+		default: null
+	},
+	jobsVancyTags: {
+		type: mongoose.Types.Array,
+		trim: true,
+		required: [true, 'jobsVancyTags is required'],
+		default: []
+	},
+	createdAt: {
+		type: Date,
+		default: null
+	},
+	updatedAt: {
+		type: Date,
+		default: null
+	}
 })
 
 export const jobsSchema: Model<JobsDTO> = mongoose.mode('jobsService', Schema)
