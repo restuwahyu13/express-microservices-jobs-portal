893a6658570223e56f70f9491fbb5d1fde079377
Anti-Malware <restuwahyu13@gmail.com>
2021-03-17 00:22:34 +0700

    add authJwt fro any route from jobs service

--- jobs-service/src/routes/route.jobs.ts
+++ jobs-service/src/routes/route.jobs.ts
@@ -1,13 +1,14 @@
 import express, { Router } from 'express'
 import { controller } from '../controllers'
 import { serviceLogger } from '../middlewares/middleware.logger'
+import { authJwt } from '../middlewares/middleware.auth'
 // import { loginValidator, emailValidator, registerValidator, tokenValidator, passwordValidator } from '../utils/util.validator'
 
 const router: Router = express.Router()
 
-router.post('/companies/:companyId/jobs', [serviceLogger('Jobs Create Service')], controller.createJobsController)
-router.get('/companies/:companyId/jobs', [serviceLogger('Login Service')], controller.resultJobsController)
-router.delete('/companies/:companyId/jobs', [serviceLogger('Forgot Service')], controller.deleteJobsController)
-router.put('/companies/:companyId/jobs', [serviceLogger('Resend Service')], controller.updateJobsController)
+router.post('/companies/:companyId/jobs', [serviceLogger('Jobs Create Service'), authJwt()], controller.createJobsController)
+router.get('/companies/:companyId/jobs', [serviceLogger('Jobs Result Service'), authJwt()], controller.resultJobsController)
+router.delete('/companies/:companyId/jobs', [serviceLogger('Jobs Delete Service'), authJwt()], controller.deleteJobsController)
+router.put('/companies/:companyId/jobs', [serviceLogger('Jobs Update Service'), authJwt()], controller.updateJobsController)
 
 export default router
