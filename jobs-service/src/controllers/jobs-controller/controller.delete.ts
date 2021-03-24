4329a78a71e367ee478919b151a6dc221f6486fd
restuwahyu13 <restuwahyu13@gmail.com>
2021-03-24 09:26:26 +0700

    update jobs controller from jobs-service

--- jobs-service/src/controllers/jobs-controller/controller.delete.ts
+++ jobs-service/src/controllers/jobs-controller/controller.delete.ts
@@ -1,7 +1,28 @@
 import { Request, Response } from 'express'
 import { setDeleteJobsPublisher } from '../../services/publisher/jobs-publisher/service.delete'
+import { initDeleteJobsSubscriber } from '../../services/subscriber/jobs-subscriber/service.delete'
+import { getResponseSubscriber } from '../../utils/util.message'
 import { streamBox } from '../../utils/util.stream'
+import { getPipelineSpeaker } from '../../utils/util.speaker'
+import { IJobs } from '../../interface/interface.jobs'
 
-export const deleteJobsController = async (req: Request, res: Response): Promise<Response<any>> => {
-	const { status, message } = setDeleteJobsPublisher()
+export const deleteJobsController = async (req: Request, res: Response): Promise<void> => {
+	const response: IJobs = await getPipelineSpeaker('speaker:delete:companies:to:jobs')
+	await setDeleteJobsPublisher({ ...response })
+	await initDeleteJobsSubscriber()
+	const { status, message } = await getResponseSubscriber()
+
+	if (status >= 400) {
+		streamBox(res, status, {
+			method: req.method,
+			status,
+			message
+		})
+	} else {
+		streamBox(res, status, {
+			method: req.method,
+			status,
+			message
+		})
+	}
 }
--- jobs-service/src/controllers/jobs-controller/controller.update.ts
+++ jobs-service/src/controllers/jobs-controller/controller.update.ts
@@ -1,7 +1,28 @@
 import { Request, Response } from 'express'
 import { setUpdateJobsPublisher } from '../../services/publisher/jobs-publisher/service.update'
+import { initUpdateMeSubscriber } from '../../services/subscriber/jobs-subscriber/service.update'
+import { getResponseSubscriber } from '../../utils/util.message'
+import { getPipelineSpeaker } from '../../utils/util.speaker'
 import { streamBox } from '../../utils/util.stream'
+import { IJobs } from '../../interface/interface.jobs'
 
-export const updateJobsController = async (req: Request, res: Response): Promise<Response<any>> => {
-	const { status, message } = setUpdateJobsPublisher()
+export const updateJobsController = async (req: Request, res: Response): Promise<void> => {
+	const response: IJobs = await getPipelineSpeaker('speaker:update:companies:to:jobs')
+	await setUpdateJobsPublisher({ ...response })
+	await initUpdateMeSubscriber()
+	const { status, message } = await getResponseSubscriber()
+
+	if (status >= 400) {
+		streamBox(res, status, {
+			method: req.method,
+			status,
+			message
+		})
+	} else {
+		streamBox(res, status, {
+			method: req.method,
+			status,
+			message
+		})
+	}
 }
