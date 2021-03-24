95dc817ab32ce2ced9a615a97225a934959e300f
restuwahyu13 <restuwahyu13@gmail.com>
2021-03-24 09:45:33 +0700

    update delete jobs subscriber from jobs-service

--- jobs-service/src/services/subscriber/jobs-subscriber/service.delete.ts
+++ jobs-service/src/services/subscriber/jobs-subscriber/service.delete.ts
@@ -1,108 +1,27 @@
 import { v4 as uuid } from 'uuid'
 import { Subscriber } from '../../../utils/util.subscriber'
 import { setResponsePublisher } from '../../../utils/util.message'
-import { profileSchema } from '../../models/model.profile'
-import { ProfilesDTO } from '../../dto/dto.profile'
-import { IJobs } from '../../interface/interface.service'
+import { jobsSchema } from '../../../models/model.job'
+import { JobsDTO } from '../../../dto/dto.job'
+import { IJobs } from '../../../interface/interface.jobs'
 
 export const initDeleteJobsSubscriber = async (): Promise<void> => {
-	const deleteJobsSubscriber = new Subscriber({ key: 'Sub Profile' })
-	const res: IJobs = await deleteJobsSubscriber.getMap('djobs:service')
+	const deleteJobsSubscriber = new Subscriber({ key: 'Jobs Delete' })
+	const res: IJobs = await deleteJobsSubscriber.getMap('jobs:delete:service')
 
 	try {
-		const checkJobsExist: ProfilesDTO[] = await profileSchema
-			.find({
-				'jobPreferences.jobId': res.jobPreferences.jobsId,
-				'$or': [
-					{ 'jobPreferences.jobInterests': { $in: [res.jobPreferences.jobInterests] } },
-					{ 'jobPreferences.workTypes': { $in: [res.jobPreferences.workTypes] } },
-					{ 'jobPreferences.workCityPreferences': { $in: [res.jobPreferences.workCityPreferences] } }
-				]
-			})
-			.lean()
+		const checkJobsExist: JobsDTO[] = await jobsSchema.findOneAndDelete({ jobId: res.jobsId }).lean()
 
-		if (checkJobsExist.length < 1) {
+		if (!checkJobsExist) {
 			await setResponsePublisher({
 				status: 404,
-				message: `value is not exist in jobInterests | workTypes | workCityPreferences, or deleted from owner`
+				message: `jobs post is not exist for this id ${res.jobsId}, or deleted from owner`
 			})
 		} else {
-			const deleteJobs: ProfilesDTO = await profileSchema.updateOne(
-				{ 'jobPreferences.jobId': res.jobPreferences.jobsId },
-				{
-					$pull: {
-						'jobPreferences.jobInterests': res.jobPreferences.jobInterests,
-						'jobPreferences.workTypes': res.jobPreferences.workTypes,
-						'jobPreferences.workCityPreferences': res.jobPreferences.workCityPreferences
-					}
-				}
-			)
-
-			if (!deleteJobs) {
-				await setResponsePublisher({
-					status: 403,
-					message: `deleted job value in jobInterests | workTypes | workCityPreferences failed`
-				})
-			} else {
-				await setResponsePublisher({
-					status: 200,
-					message: `deleted job value in jobInterests | workTypes | workCityPreferences successfully`
-				})
-			}
-		}
-	} catch (error) {
-		await setResponsePublisher({
-			status: 500,
-			message: `internal server error: ${error}`
-		})
-	}
-}
-
-export const initUpdateJobsSubscriber = async (): Promise<void> => {
-	const updateJobsSubscriber = new Subscriber({ key: 'Sub Profile' })
-	const res: IJobs = await updateJobsSubscriber.getMap('ujobs:service')
-
-	try {
-		const checkJobsExist: ProfilesDTO[] = await profileSchema
-			.find({
-				'jobPreferences.jobId': res.jobPreferences.jobsId,
-				'$or': [
-					{ 'jobPreferences.jobInterests': { $in: [...res.jobPreferences.jobInterests] } },
-					{ 'jobPreferences.workTypes': { $in: [...res.jobPreferences.workTypes] } },
-					{ 'jobPreferences.workCityPreferences': { $in: [...res.jobPreferences.workCityPreferences] } }
-				]
-			})
-			.lean()
-
-		if (checkJobsExist.length > 0) {
 			await setResponsePublisher({
-				status: 409,
-				message: `value already exist in jobInterests | workTypes | workCityPreferences, or deleted from owner`
+				status: 200,
+				message: `deleted job post successfully for this id ${res.jobsId}`
 			})
-		} else {
-			const updateJobs: ProfilesDTO = await profileSchema.updateOne(
-				{ 'jobPreferences.jobId': res.jobPreferences.jobsId },
-				{
-					$set: { 'jobs.salaryExpectation': res.jobPreferences.salaryExpectation },
-					$addToSet: {
-						'jobPreferences.jobInterests': { $each: [...res.jobPreferences.jobInterests] },
-						'jobPreferences.workTypes': { $each: [...res.jobPreferences.workTypes] },
-						'jobPreferences.workCityPreferences': { $each: [...res.jobPreferences.workCityPreferences] }
-					}
-				}
-			)
-
-			if (!updateJobs) {
-				await setResponsePublisher({
-					status: 403,
-					message: `updated job value in jobInterests | workTypes | workCityPreferences failed`
-				})
-			} else {
-				await setResponsePublisher({
-					status: 200,
-					message: `updated job value in jobInterests | workTypes | workCityPreferences successfully`
-				})
-			}
 		}
 	} catch (error) {
 		await setResponsePublisher({
